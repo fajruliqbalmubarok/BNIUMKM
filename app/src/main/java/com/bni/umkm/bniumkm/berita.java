e7b68266016d0b9a7bd8d0c29e46751d2e9d1151
fajruliqbalmubarok <iqbalfajrul21@gmail.com>
2017-03-07 23:50:32 +0700

    data baru

---
+++ app/src/main/java/com/bni/umkm/bniumkm/berita.java
@@ -0,0 +1,19 @@
+package com.bni.umkm.bniumkm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class berita extends Fragment {
+    public berita() {
+        // Required empty publ  ic constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.berita, container, false);
+    }
+}
