1f385ba12ea10954adc53879bfb0084604a12d49
fajruliqbalmubarok <iqbalfajrul21@gmail.com>
2017-03-08 17:29:30 +0700

    layout

--- app/src/main/java/com/bni/umkm/bniumkm/user/berandauser.java
+++ app/src/main/java/com/bni/umkm/bniumkm/user/berandauser.java
@@ -1,8 +1,14 @@
-package com.bni.umkm.bniumkm;
+package com.bni.umkm.bniumkm.user;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager; //import ViewPager, viewpager berfungsi untuk mengontrol posisi tab
@@ -13,15 +19,21 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bni.umkm.bniumkm.DataSQLite.DataHelper;
+import com.bni.umkm.bniumkm.MainActivity;
+import com.bni.umkm.bniumkm.R;
+import com.bni.umkm.bniumkm.login;
+import com.bni.umkm.bniumkm.config.koneksi;
+import com.bni.umkm.bniumkm.tabadapter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class berandauser extends AppCompatActivity {
     private TabLayout tabLayout; //Deklarasi Tab Layout
 
     private ViewPager viewPager; //Deklarasi view pager
     private DrawerLayout dr;
     private Toolbar tl;
 
+    DataHelper dbcenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         tl = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tl);
         ProsesNavigasi();
-
+        dbcenter = new DataHelper(this);
         // tablayout
         tabLayout = (TabLayout) findViewById(R.id.tabLayout); //mengenalkan komponen tab layout yang berada di activity_main ke MainActivity.java
         tabLayout.addTab(tabLayout.newTab().setText("Berita")); //Nama menu tab yang pertama
@@ -73,16 +85,22 @@ public class MainActivity extends AppCompatActivity {
                 int id = menuItem.getItemId();
 
                 switch (id) {
-                    case R.id.about:
-                        Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+                    case R.id.profile:
+                        Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_SHORT).show();
                         dr.closeDrawers();
                         break;
-                    case R.id.login:
-                        Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+                    case R.id.record:
+                        Toast.makeText(getApplicationContext(), "Record", Toast.LENGTH_SHORT).show();
                         dr.closeDrawers();
-                        Intent i=null;
-                        i = new Intent(MainActivity.this, login.class);
-                        startActivity(i);
+                        break;
+                    case R.id.transaksi:
+                        Toast.makeText(getApplicationContext(), "Transaksi", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+                        dr.closeDrawers();
+                       logout();
                 }
                 return true;
             }
@@ -107,5 +125,49 @@ public class MainActivity extends AppCompatActivity {
         dr.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
     }
-}
+    private void logout() {
+        // Munculkan alert dialog apabila user ingin keluar aplikasi
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Metu Temenana?");
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
 
+                        // Getting out
+                        SharedPreferences preferences = getSharedPreferences(koneksi.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+                        //Getting editor
+                        SharedPreferences.Editor editor = preferences.edit();
+
+                        // put nilai false untuk login
+                        editor.putBoolean(koneksi.LOGGEDIN_SHARED_PREF, false);
+
+                        // put nilai untuk username
+                        editor.putString(koneksi.EMAIL_SHARED_PREF, "");
+
+                        //Simpan ke haredpreferences
+                        editor.commit();
+                        SQLiteDatabase db = dbcenter.getWritableDatabase();
+                        db.execSQL("delete from member where 1");
+                        Intent launchNextActivity;
+                        launchNextActivity = new Intent(berandauser.this, MainActivity.class);
+                        launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(launchNextActivity);
+                    }
+                });
+        // Pilihan jika NO
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+        // Tampilkan alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
